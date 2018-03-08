52d01e06e44aac794baab9c26496871f03497933
Dfrost-Nid <36641266+dfrostNid@users.noreply.github.com>
2018-03-08 18:34:03 +0530

    Minor text changes with map changes

--- js/main.js
+++ js/main.js
@@ -14,13 +14,7 @@
       }
     }
   });
-
-  
-  
-  
-  
-  
-  
+ 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
@@ -93,4 +87,42 @@
   initializeClock('clockdiv', deadline); */
   //Coountdown timer ends
 
+
+
 )(jQuery); // End of use strict
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicmpkZXNobXVraCIsImEiOiJjajAzZm1nNWMwYXV4MndwM3Rsb3p6cHNnIn0.AssOKgLheiWRLzKSW4UWUg';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/rjdeshmukh/cje8st0pi7yqc2snrbp4rtq0m'
+});
+
+
+map.on('load', function() {
+    map.loadImage('img/map/map-marker.svg', function(error, image) {
+        if (error) throw error;
+        map.addImage('nid-marker', image);
+        map.addLayer({
+            "id": "points",
+            "type": "symbol",
+            "source": {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [77.535793, 13.034548],
+                            "zoom": 16.21
+                        }
+                    }]
+                }
+            },
+            "layout": {
+                "icon-image": "nid-marker",
+                "icon-size": 0.35
+            }
+        });
+    });
+});
