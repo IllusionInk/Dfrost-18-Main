d6fc1372abaf01c118860b3977f3ee22678bf986
Chanch95 <Chanchal13030@iiitd.ac.in>
2018-03-08 18:18:33 +0530

    chanch-8feb-6:18

--- js/main.js
+++ js/main.js
@@ -45,21 +45,7 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-  //Countdown timer
-  function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
+
 }
 
  /*  function initializeClock(id, endtime) {
@@ -94,3 +80,5 @@
   //Coountdown timer ends
 
 )(jQuery); // End of use strict
+
+
